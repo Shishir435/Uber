eba12c273845036de65533f4b4b9331442fd1dc9
shishir435 <shishirchaurasiya435@gmail.com>
2024-08-18 23:06:02 +0530

    chore: install eslint and prettier

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { StatusBar, Text} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { StatusBar, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-      <SafeAreaView className="flex-1 items-center justify-center bg-white">
-        <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
-        <StatusBar  />
-      </SafeAreaView>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500">
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar />
+    </SafeAreaView>
   );
 }
