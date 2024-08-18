55162440238bd4ed32a0f24509fd060bddac56ac
shishir435 <shishirchaurasiya435@gmail.com>
2024-08-19 04:15:41 +0530

    feat: finish welcome screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,9 +1,14 @@
 import { router } from "expo-router";
-import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import React, { useRef, useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import Swiper from "react-native-swiper";
+import { welcome } from "@/constants";
+import CustomButton from "@/components/CustomButton";
 const Welcome = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === welcome.length - 1;
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -12,6 +17,36 @@ const Welcome = () => {
       >
         <Text className="text-black text-base font-JakartaBold">Skip</Text>
       </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={<View className="w-8 h-1 mx-1 bg-[#E2E8F0] rounded-full" />}
+        activeDot={<View className="w-8 h-1 mx-1 bg-[#0286ff] rounded-full" />}
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {welcome.map((item) => (
+          <View key={item.id} className="flex justify-center items-center p-5">
+            <Image source={item.image} className="w-full h-[300px]" />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="text-base font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10 mb-5"
+      />
     </SafeAreaView>
   );
 };
